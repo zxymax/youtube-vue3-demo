d1ca1b045890085eb9f1f925946241215d42515f
zxymax <497084653@qq.com>
2022-04-12 10:44:30 +0800

    Vue 3 Composition API Tutorial demo

---
+++ src/pages/Home.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+const counter = ref(0)
+const counterTitle = ref('My counter Title')
+
+const counterData = reactive({
+  count: 0,
+  title: 'My counter'
+})
+
+
+const increaseCounter = () => {
+  counter.value++
+}
+
+const decreaseCounter = () => {
+  counter.value--
+}
+
+const increaseCounter2 = () => {
+  counterData.count++
+}
+
+const decreaseCounter2 = () => {
+  counterData.count--
+}
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{counterTitle === '' ? 'My counter Title' : counterTitle}}</h1>
+    <div>
+      <button @click="decreaseCounter"> - </button>
+      <span>{{counter}}</span>
+      <button @click="increaseCounter"> + </button>
+    </div>
+    <div>
+      <button @click="decreaseCounter2">decreaseCounter2</button>
+      <span>{{counterData.count}}</span>
+      <button @click="increaseCounter2">increaseCounter2</button>
+    </div>
+    <div class="edit">
+      <h3>Edit counter title: </h3>
+      <input type="text" v-model="counterTitle" />
+    </div>
+  </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../pages/Home.vue'
+import About from '../pages/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  }
+]
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
